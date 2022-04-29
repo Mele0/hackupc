7ad8a0911e224d0436c70ab4c7beb84e231a4f93
Your Name <you@example.com>
2022-04-30 01:30:26 +0200

    ADD: create tables

--- SERVER/index.js
+++ SERVER/index.js
@@ -4,18 +4,31 @@ require('dotenv').config();
 const port = process.env.PORT
 const host = process.env.HOST
 const user = process.env.USER
+
 const password = process.env.PASSWORD
 
-const mysql = require('mysql')
+const mysql = require('mysql2')
 
 
 var connection = mysql.createConnection({
-  host     : host,
-  user     : user,
-  password : password
+  host     : 'localhost',
+  user     : 'username',
+  password : 'eljuanketasenlahackupc',
 });
 
-connection.connect();
+
+connection.connect(function(err){
+  connection.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+  });
+  var sql = "CREATE TABLE money (name VARCHAR(255), address VARCHAR(255))";
+  connection.query(sql, function (err, result) {
+      if (err) throw err;
+    }
+  )
+
+
+});
 
 
 app.listen(port, () => {
