9245f77d8c47c99c1f45d22cc8d9ff0a5ad02980
Your Name <you@example.com>
2022-04-29 23:42:59 +0200

    ADD: mysql init

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 require('dotenv').config();
-const port = process.env.port
+const port = process.env.PORT
+const host = process.env.HOST
+const user = process.env.USER
+const password = process.env.PASSWORD
+
 const mysql = require('mysql')
 
 
+var connection = mysql.createConnection({
+  host     : host,
+  user     : user,
+  password : password
+});
 
-
+connection.connect();
 
 
 app.listen(port, () => {
