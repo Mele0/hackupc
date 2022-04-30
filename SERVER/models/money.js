b994f729ac76209a8cb6565c4bc86af1cb5170bb
davidF2801 <dfornosf@gmail.com>
2022-04-30 06:16:57 +0200

    Tabla dinero funciona

--- SERVER/models/money.js
+++ SERVER/models/money.js
@@ -6,7 +6,7 @@ const money_result = require('../helpers/db_connect')
 async function get_money_by_id(user){
     con = money_result.db_connection()  
     console.log('hola')  
-    var  sql = "SELECT * FROM money WHERE id = " + mysql.escape(user)
+    var  sql = "SELECT balance FROM money WHERE id = " + mysql.escape(user)
     result = await money_result.db_query(sql, con)
     console.log(result)
     return result
