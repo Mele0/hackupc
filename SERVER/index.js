016ecda8ca10ffc591d5f7a3f607bfa88adb5a9a
davidF2801 <dfornosf@gmail.com>
2022-04-30 04:42:19 +0200

    BORRADO DE COSAS INUTILES

--- SERVER/index.js
+++ SERVER/index.js
@@ -32,24 +32,10 @@ connection.connect(function(err){
       if (err) throw err;
   });*/
 
-  console.log("table created!");
-  var sqlmoney = "INSERT INTO money (id, balance) VALUES ('49271168Q',3890)";
-  connection.query(sqlmoney, function (err, result) {
-      if (err) throw err;
-  });
-
-  var sqlexpenses = "INSERT INTO expenses (id, input, time) VALUES ('49271168Q',-1000, '2018-09-08 17:51:04.777')";
-
-
-  /*var sqlmoney = "CREATE TABLE money (id VARCHAR(255), balance FLOAT(10))";
-  connection.query(sqlmoney, function (err, result) {
-      if (err) throw err;
-  });
-  console.log("table created!"); 
   var sqlexpenses = "CREATE TABLE expenses (id VARCHAR(255), input FLOAT(10), time DATETIME(2))";
   connection.query(sqlexpenses, function (err, result) {
       if (err) throw err;
-  });*/
+  });
 
   console.log("table created!");
   var sqlmoney = "INSERT INTO money (id, balance) VALUES ('49271168Q',3890)";
@@ -61,7 +47,8 @@ connection.connect(function(err){
   connection.query(sqlexpenses, function (err, result) {
       if (err) throw err;
   });
-  console.log("value created!");  
+  console.log("value created!"); 
+
 
 });
 
@@ -110,11 +97,11 @@ app.get('/', async function(req, res){
 
   result = await dbQuery("SELECT * FROM expenses");
   var res_string = JSON.stringify(result);
-  // result = await dbQuery("SELECT * FROM money");
-  // var res_string = JSON.stringify(result);
+  result = await dbQuery("SELECT * FROM money");
+  var res_string = JSON.stringify(result);
 
 
-  // res.end(res_string)
+  res.end(res_string)
 
 
 });
