d0816e2eb6c4e861895b8a70530d6c39f5790fb6
davidF2801 <dfornosf@gmail.com>
2022-04-30 22:37:25 +0200

    BIBLIOSTUDY

--- SERVER/index.js
+++ SERVER/index.js
@@ -7,17 +7,13 @@ const expenses = require('./routes/expenses.js');
 const con = require('./helpers/db_connect.js')
 const td = require('./testdata.js')
 const port = process.env.PORT
-const host = process.env.HOST
-const user = process.env.USER
-const password = process.env.PASSWORD
-const mysql = require('mysql2')
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json()); 
 
 var connection = con.db_connection();
 connection.connect(function(err){
   console.log("Connected!");
-  td.create_test_data(true);
+  td.create_test_data(false);
 });
 
 app.use('/money', money)
