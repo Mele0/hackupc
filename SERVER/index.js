2ddb36537c737984e74986a46f3e965680a66235
davidF2801 <dfornosf@gmail.com>
2022-04-30 03:49:26 +0200

    UPDATE: TABLAS CREADAS

--- SERVER/index.js
+++ SERVER/index.js
@@ -20,19 +20,32 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
   console.log("Connected!");  
-  /*var sql = "CREATE TABLE money (name VARCHAR(255))";
-  connection.query(sql, function (err, result) {
+  /*var sqlmoney = "CREATE TABLE money (id VARCHAR(255), balance FLOAT(10))";
+  connection.query(sqlmoney, function (err, result) {
       if (err) throw err;
   });
-  console.log("table created!");  */
+  console.log("table created!"); 
+  var sqlexpenses = "CREATE TABLE expenses (id VARCHAR(255), input FLOAT(10), time DATETIME(2))";
+  connection.query(sqlexpenses, function (err, result) {
+      if (err) throw err;
+  });*/
 
-  var sql = "INSERT INTO money (name) VALUES ('hola')";
-  connection.query(sql, function (err, result) {
+  console.log("table created!");
+  var sqlmoney = "INSERT INTO money (id, balance) VALUES ('49271168Q',3890)";
+  connection.query(sqlmoney, function (err, result) {
+      if (err) throw err;
+  });
+
+  var sqlexpenses = "INSERT INTO expenses (id, input, time) VALUES ('49271168Q',-1000, '2018-09-08 17:51:04.777')";
+
+  connection.query(sqlexpenses, function (err, result) {
       if (err) throw err;
   });
   console.log("value created!");  
 
 });
+
+
 
 function dbQuery(databaseQuery) {
   return new Promise(data => {
@@ -74,7 +87,7 @@ var stuff = '';
 
 app.get('/', async function(req, res){
 
-  result = await dbQuery("SELECT * FROM money");
+  result = await dbQuery("SELECT * FROM expenses");
   var res_string = JSON.stringify(result);
 
   res.end(res_string)
