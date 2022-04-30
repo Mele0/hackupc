e68829ec9d428c3f3055888ccf9c2550ce6d22ad
Your Name <you@example.com>
2022-04-30 02:15:15 +0200

    ADD: get info from table

--- SERVER/index.js
+++ SERVER/index.js
@@ -10,6 +10,7 @@ const password = process.env.PASSWORD
 const mysql = require('mysql2')
 
 
+
 var connection = mysql.createConnection({
   host     : host,
   user     : user,
@@ -20,16 +21,46 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
   connection.query("CREATE DATABASE mydb", function (err, result) {
     if (err) throw err;
+    console.log("hola")
   });
-  var sql = "CREATE TABLE money (name VARCHAR(255), address VARCHAR(255))";
+  var sql = "USE mydb";
   connection.query(sql, function (err, result) {
       if (err) throw err;
-    }
-  )
-
-
+    });
+  var sql = "CREATE TABLE money (name VARCHAR(255))";
+  connection.query(sql, function (err, result) {
+      if (err) throw err;
+  });
+  var sql = "INSERT INTO money (hola)";
+  connection.query(sql, function (err, result) {
+      if (err) throw err;
+  });
+  
 });
+function get_info(callback){
+      
+  var sql = "SELECT * FROM money";
 
+  connection.query(sql, function(err, results){
+        if (err){ 
+          throw err;
+        }
+        console.log(results[0].objid);
+        stuff_i_want = results[0].objid;  
+        return callback(results[0].objid);  
+  })
+}
+
+
+app.get('/', async function(req, res){
+  var results = '';
+
+  get_info(function(result){
+    results = result
+  })
+
+  res.end(results)
+});  
 
 app.listen(port, () => {
 
